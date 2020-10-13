265f4b2c1971c28dc408272a02cc941c7afc7a87
Carleigh Leyman <leyman@bc.edu>
2020-10-13 09:59:13 -0400

    6.4 completed

---
+++ WeatherGift/APIkeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIkeys.swift
+//  WeatherGift
+//
+//  Created by Carleigh Leyman on 10/10/20.
+//
+
+import Foundation
+
+struct APIkeys {
+    static let googlePlacesKey = "AIzaSyDYj6tRlf2NW_KLzVU_sRiHveRBlhC-nD4"
+}
+
---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Carleigh Leyman on 10/8/20.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
